a9918feb741ae8f8ffb3b411ed61045c338cb2c9
Danilo Urrutia <danilo@dreia.com>
2026-03-19 14:46:46 -0500

    feat: update email templates to use dynamic RootURL for links

--- cmd/install.go
+++ cmd/install.go
@@ -249,10 +249,10 @@ func installCampaign(campTplID, archiveTplID int, q *models.Queries) {
 		"No Reply <noreply@yoursite.com>",
 		`<h3>Hi {{ .Subscriber.FirstName }}!</h3>
 		<p>This is a test e-mail campaign. Your second name is {{ .Subscriber.LastName }} and you are from {{ .Subscriber.Attribs.city }}.</p>
-		<p>Here is a <a href="https://listmonk.app@TrackLink">tracked link</a>.</p>
+		<p>Here is a <a href="{{ RootURL }}@TrackLink">tracked link</a>.</p>
 		<p>Use the link icon in the editor toolbar or when writing raw HTML or Markdown,
 			simply suffix @TrackLink to the end of a URL to turn it into a tracking link. Example:</p>
-		<pre>&lt;a href=&quot;https:/&zwnj;/listmonk.app&#064;TrackLink&quot;&gt;&lt;/a&gt;</pre>
+		<pre>&lt;a href=&quot;{{ RootURL }}&#064;TrackLink&quot;&gt;&lt;/a&gt;</pre>
 		<p>For help, refer to the <a href="https://listmonk.app/docs">documentation</a>.</p>
 		`,
 		nil,
--- cmd/templates.go
+++ cmd/templates.go
@@ -24,7 +24,7 @@ const (
 		<h3>Sub heading</h3>
 		<p>Nam luctus dui non placerat mattis. Morbi non accumsan orci, vel interdum urna. Duis faucibus id nunc ut euismod. Curabitur et eros id erat feugiat fringilla in eget neque. Aliquam accumsan cursus eros sed faucibus.</p>
 
-		<p>Here is a link to <a href="https://listmonk.app" target="_blank">listpocket</a>.</p>`
+		<p>Here is a link to <a href="{{ RootURL }}" target="_blank">listpocket</a>.</p>`
 )
 
 var (
